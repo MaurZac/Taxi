25221e438db29b735aa359a6c460efb5bdbbfbdd
MaurZac <zaratem@globalhitss.com>
2024-01-21 21:43:46 -0600

    Initial Commit

---
+++ Taxi/TaxiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxiApp.swift
+//  Taxi
+//
+//  Created by Mauricio Zarate Chula on 1/21/24.
+//
+
+import SwiftUI
+
+@main
+struct TaxiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
